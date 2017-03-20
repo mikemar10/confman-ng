c3dc94bad7884700ca55cfd8b9db847dc0e2a795
Mike Martin <mike@unsupported.me>
2017-03-20 00:16:19 -0500

    Go fmt cleanup

--- bundle/bundle.go
+++ bundle/bundle.go
@@ -1,113 +1,113 @@
 package main
 
 import (
-    "crypto/sha256"
-    "fmt"
-    "github.com/davecheney/xattr"
-    "io"
-    "io/ioutil"
-    "log"
-    "os"
-    "os/exec"
-    "os/user"
-    "path/filepath"
-    "runtime"
-    "strconv"
-    "strings"
-    "syscall"
+	"crypto/sha256"
+	"fmt"
+	"github.com/davecheney/xattr"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"syscall"
 )
 
 func main() {
-    if runtime.GOOS != "linux" {
-        log.Fatal("Linux is the only currently supported operating system")
-    }
-    target_path := strings.TrimRight(os.Args[1], "/")
-    tag_files(target_path)
-    make_squashfs(target_path)
+	if runtime.GOOS != "linux" {
+		log.Fatal("Linux is the only currently supported operating system")
+	}
+	target_path := strings.TrimRight(os.Args[1], "/")
+	tag_files(target_path)
+	make_squashfs(target_path)
 }
 
 func tag_files(target_path string) {
-    all_users := make(map[string]uint32)
-    all_groups := make(map[string]uint32)
+	all_users := make(map[string]uint32)
+	all_groups := make(map[string]uint32)
 
-    filepath.Walk(target_path, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            log.Fatal(err)
-        }
+	filepath.Walk(target_path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        stat, ok := info.Sys().(*syscall.Stat_t)
-        if !ok {
-            log.Fatal("Failed type assertion of type syscall.Stat_t")
-        }
-        username := get_username(stat.Uid)
-        groupname := get_groupname(stat.Gid)
+		stat, ok := info.Sys().(*syscall.Stat_t)
+		if !ok {
+			log.Fatal("Failed type assertion of type syscall.Stat_t")
+		}
+		username := get_username(stat.Uid)
+		groupname := get_groupname(stat.Gid)
 
-        switch mode := info.Mode(); {
-            case mode.IsRegular():
-                xattr.Setxattr(path, "user.sha256sum", sha256sum(path))
-                fallthrough
-            case mode.IsDir():
-                xattr.Setxattr(path, "user.user", []byte(username))
-                xattr.Setxattr(path, "user.group", []byte(groupname))
-        }
-        all_users[username] = stat.Uid
-        all_groups[groupname] = stat.Gid
-        return nil
-    })
+		switch mode := info.Mode(); {
+		case mode.IsRegular():
+			xattr.Setxattr(path, "user.sha256sum", sha256sum(path))
+			fallthrough
+		case mode.IsDir():
+			xattr.Setxattr(path, "user.user", []byte(username))
+			xattr.Setxattr(path, "user.group", []byte(groupname))
+		}
+		all_users[username] = stat.Uid
+		all_groups[groupname] = stat.Gid
+		return nil
+	})
 
-    persist_map(target_path + "/.users", all_users)
-    persist_map(target_path + "/.groups", all_groups)
+	persist_map(target_path+"/.users", all_users)
+	persist_map(target_path+"/.groups", all_groups)
 }
 
 func get_username(_uid uint32) string {
-    uid := strconv.FormatUint(uint64(_uid), 10)
-    username, err := user.LookupId(uid)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return username.Username
+	uid := strconv.FormatUint(uint64(_uid), 10)
+	username, err := user.LookupId(uid)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return username.Username
 }
 
 func get_groupname(_gid uint32) string {
-    gid := strconv.FormatUint(uint64(_gid), 10)
-    groupname, err := user.LookupGroupId(gid)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return groupname.Name
+	gid := strconv.FormatUint(uint64(_gid), 10)
+	groupname, err := user.LookupGroupId(gid)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return groupname.Name
 }
 
 func sha256sum(path string) []byte {
-    file, err := os.Open(path);
-    if err != nil {
-        log.Fatal(err)
-    }
-    hash := sha256.New()
-    if _, err := io.Copy(hash, file); err != nil {
-        log.Fatal(err)
-    }
-    if err := file.Close(); err != nil {
-        log.Fatal(err)
-    }
-    return []byte(fmt.Sprintf("%x", hash.Sum(nil)))
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		log.Fatal(err)
+	}
+	if err := file.Close(); err != nil {
+		log.Fatal(err)
+	}
+	return []byte(fmt.Sprintf("%x", hash.Sum(nil)))
 }
 
 func persist_map(path string, items map[string]uint32) error {
-    data := make([]byte, 0)
-    for key, value := range items {
-        s := fmt.Sprintf("%s:%d\n", key, value)
-        data = append(data, s...)
-    }
+	data := make([]byte, 0)
+	for key, value := range items {
+		s := fmt.Sprintf("%s:%d\n", key, value)
+		data = append(data, s...)
+	}
 
-    ioutil.WriteFile(path, data, 0444)
-    return nil
+	ioutil.WriteFile(path, data, 0444)
+	return nil
 }
 
 func make_squashfs(target_path string) {
-    cmd := exec.Command("mksquashfs", target_path, "target.squashfs", "-no-progress")
-    output, err := cmd.CombinedOutput()
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf("%s\n", output)
+	cmd := exec.Command("mksquashfs", target_path, "target.squashfs", "-no-progress")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", output)
 }
--- unbundle/unbundle.go
+++ unbundle/unbundle.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
-    "os/exec"
-    "runtime"
-    "time"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	"time"
 )
 
 func main() {
-    if runtime.GOOS != "linux" {
-        log.Fatal("Linux is the only currently supported operating system")
-    }
-    squashfs_path := os.Args[1]
-    tmp_mount_path := create_tmp_dir()
-    mount_squashfs(squashfs_path, tmp_mount_path)
-    cleanup_tmp_mount(tmp_mount_path)
+	if runtime.GOOS != "linux" {
+		log.Fatal("Linux is the only currently supported operating system")
+	}
+	squashfs_path := os.Args[1]
+	tmp_mount_path := create_tmp_dir()
+	mount_squashfs(squashfs_path, tmp_mount_path)
+	cleanup_tmp_mount(tmp_mount_path)
 }
 
 func create_tmp_dir() string {
-    tmpdir := fmt.Sprint(os.TempDir(), "/confman-ng-", time.Now().UnixNano())
-    err := os.Mkdir(tmpdir, 0700)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return tmpdir
+	tmpdir := fmt.Sprint(os.TempDir(), "/confman-ng-", time.Now().UnixNano())
+	err := os.Mkdir(tmpdir, 0700)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tmpdir
 }
 
 func mount_squashfs(squashfs_path, mount_path string) {
-    cmd := exec.Command("mount", squashfs_path, mount_path)
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
+	cmd := exec.Command("mount", squashfs_path, mount_path)
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func cleanup_tmp_mount(mount_path string) {
-    cmd := exec.Command("umount", mount_path)
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
-    if err := os.Remove(mount_path); err != nil {
-        log.Fatal(err)
-    }
+	cmd := exec.Command("umount", mount_path)
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	if err := os.Remove(mount_path); err != nil {
+		log.Fatal(err)
+	}
 }
