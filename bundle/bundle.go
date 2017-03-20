bacbd65d20e4a7ff4369e296e97d4ced7580803e
Mike Martin <mike@unsupported.me>
2017-03-19 23:33:00 -0500

    Cleanup refactor; Adds basic error checking

--- bundle/bundle.go
+++ bundle/bundle.go
@@ -16,7 +16,79 @@ import (
     "syscall"
 )
 
-func write_map(path string, items map[string]uint32) error {
+func main() {
+    target_path := strings.TrimRight(os.Args[1], "/")
+    tag_files(target_path)
+    make_squashfs(target_path)
+}
+
+func tag_files(target_path string) {
+    all_users := make(map[string]uint32)
+    all_groups := make(map[string]uint32)
+
+    filepath.Walk(target_path, func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        stat, ok := info.Sys().(*syscall.Stat_t)
+        if !ok {
+            log.Fatal("Failed type assertion of type syscall.Stat_t")
+        }
+        username := get_username(stat.Uid)
+        groupname := get_groupname(stat.Gid)
+
+        switch mode := info.Mode(); {
+            case mode.IsRegular():
+                xattr.Setxattr(path, "user.sha256sum", sha256sum(path))
+                fallthrough
+            case mode.IsDir():
+                xattr.Setxattr(path, "user.user", []byte(username))
+                xattr.Setxattr(path, "user.group", []byte(groupname))
+        }
+        all_users[username] = stat.Uid
+        all_groups[groupname] = stat.Gid
+        return nil
+    })
+
+    persist_map(target_path + "/.users", all_users)
+    persist_map(target_path + "/.groups", all_groups)
+}
+
+func get_username(_uid uint32) string {
+    uid := strconv.FormatUint(uint64(_uid), 10)
+    username, err := user.LookupId(uid)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return username.Username
+}
+
+func get_groupname(_gid uint32) string {
+    gid := strconv.FormatUint(uint64(_gid), 10)
+    groupname, err := user.LookupGroupId(gid)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return groupname.Name
+}
+
+func sha256sum(path string) []byte {
+    file, err := os.Open(path);
+    if err != nil {
+        log.Fatal(err)
+    }
+    hash := sha256.New()
+    if _, err := io.Copy(hash, file); err != nil {
+        log.Fatal(err)
+    }
+    if err := file.Close(); err != nil {
+        log.Fatal(err)
+    }
+    return []byte(fmt.Sprintf("%x", hash.Sum(nil)))
+}
+
+func persist_map(path string, items map[string]uint32) error {
     data := make([]byte, 0)
     for key, value := range items {
         s := fmt.Sprintf("%s:%d\n", key, value)
@@ -27,54 +99,11 @@ func write_map(path string, items map[string]uint32) error {
     return nil
 }
 
-func main() {
-    target := strings.TrimRight(os.Args[1], "/")
-    all_users := make(map[string]uint32)
-    all_groups := make(map[string]uint32)
-    fmt.Printf("Adding metadata...\n")
-
-    preserve_metadata := func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            fmt.Println(err)
-            return nil
-        }
-
-        stat, ok := info.Sys().(*syscall.Stat_t)
-        if !ok {
-            fmt.Printf("Not a syscall.Stat_t???\n")
-            return nil
-        }
-        username, _ := user.LookupId(strconv.FormatUint(uint64(stat.Uid), 10))
-        groupname, _ := user.LookupGroupId(strconv.FormatUint(uint64(stat.Gid), 10))
-        fmt.Printf("%s:%s %s\n", username.Username, groupname.Name, path)
-
-        switch mode := info.Mode(); {
-            case mode.IsDir():
-                xattr.Setxattr(path, "user.user", []byte(username.Username))
-                xattr.Setxattr(path, "user.group", []byte(groupname.Name))
-            case mode.IsRegular():
-                xattr.Setxattr(path, "user.user", []byte(username.Username))
-                xattr.Setxattr(path, "user.group", []byte(groupname.Name))
-                f, _ := os.Open(path)
-                h := sha256.New()
-                io.Copy(h, f)
-                f.Close()
-                fmt.Printf("%x\n", h.Sum(nil))
-                sum := []byte(fmt.Sprintf("%x", h.Sum(nil)))
-                xattr.Setxattr(path, "user.sha256sum", sum)
-        }
-        all_users[username.Username] = stat.Uid
-        all_groups[groupname.Name] = stat.Gid
-        return nil
-    }
-
-    filepath.Walk(target, preserve_metadata)
-    write_map(target + "/.users", all_users)
-    write_map(target + "/.groups", all_groups)
-    cmd := exec.Command("mksquashfs", target, "target.squashfs", "-no-progress")
-    stdoutStderr, err := cmd.CombinedOutput()
+func make_squashfs(target_path string) {
+    cmd := exec.Command("mksquashfs", target_path, "target.squashfs", "-no-progress")
+    output, err := cmd.CombinedOutput()
     if err != nil {
         log.Fatal(err)
     }
-    fmt.Printf("%s\n", stdoutStderr)
+    fmt.Printf("%s\n", output)
 }
